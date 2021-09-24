eb122df19ec33a9e01c7eb3b4e3f738e072a612d
Rahul <r.dev2000@gmail.com>
2021-09-24 22:17:31 +0530

    Implement HTTP Server
    Server listens and serves with the host app's router as the handler. It also uses host app's logger for logging.

--- app/app.go
+++ app/app.go
@@ -1,8 +1,13 @@
 package app
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -11,19 +16,67 @@ import (
 	"github.com/killtheverse/nitd-results/config"
 )
 
+// Structure for the app
+// It contains the serverAddress, router, Database client and the logger
 type App struct {
-	Router		*mux.Router
-	DBClient	*mongo.Client
-	logger		*log.Logger
+	serverAddress	string
+	Router			*mux.Router
+	DBClient		*mongo.Client
+	logger			*log.Logger
 }
 
+// Configure the app and run
 func ConfigAndRun(config *config.Config) {
 	app := new(App)
-	app.Initialize(config)
+	app.initialize(config)
+	app.run()
 }
 
-func(app *App) Initialize(config *config.Config) {
+// Initialize the app
+func(app *App) initialize(config *config.Config) {
+	app.serverAddress = config.ServerAddress
 	app.Router = mux.NewRouter()
 	app.logger = log.New(os.Stdout, "results-app ", log.LstdFlags)
 	app.DBClient = db.Connect(config.DBName, config.DBURI, app.logger)
+}
+
+// Register the routes in the router
+func(app *App) setupRouters() {
+	// TODO:Implement
+}
+
+// Run will start the http server
+func(app *App) run() {
+	server := http.Server{
+		Addr: app.serverAddress,		// configure the bind address
+		Handler: app.Router,			// set the default handler
+		ErrorLog: app.logger,			// set the logger for the server
+		ReadTimeout: 5*time.Second,		// max time to read request from the client
+		WriteTimeout: 10*time.Second,	// max time to write response to the client
+		IdleTimeout: 120*time.Second,	// max time for conncections using TCP Keep-Alive
+	}
+
+	go func() {
+		app.logger.Println("Starting the server on:", app.serverAddress)
+		err := server.ListenAndServe()
+		if err != nil {
+			app.logger.Fatal("[ERROR] Can't start server:", err)
+		}
+	}()
+
+	// Signals for shutting down the server
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	
+	// Block until a signal is recieved
+	sig := <-sigs
+	app.logger.Printf("Trapped signal:%v\nShutting down the server", sig)
+
+	// Disconnect the MongoDB client
+	db.Disconnect(app.DBClient, app.logger)
+
+	// Shutdown the server, waiting for max 30 seconds
+	app.logger.Print("Gracefully stopping server")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(ctx)
 }
--- config/config.go
+++ config/config.go
@@ -6,14 +6,16 @@ import (
 
 // Server configeration structure
 type Config struct {
-	DBUser		string
-	DBPassword	string
-	DBName		string
-	DBURI		string	// mongodb connection URI
+	ServerAddress	string
+	DBUser			string
+	DBPassword		string
+	DBName			string
+	DBURI			string	// mongodb connection URI
 }
 
 // initialize will read the environment variables and store them in the config struct
 func (config *Config) initialize() {
+	config.ServerAddress = os.Getenv("SERVER_ADDRESS")
 	config.DBUser = os.Getenv("MONGODB_USER")
 	config.DBPassword = os.Getenv("MONGODB_PASSWORD")
 	config.DBName = os.Getenv("MONGODB_NAME")
